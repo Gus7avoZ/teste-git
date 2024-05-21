13b7de0c55e4177dad25a04e91e2019c6c3b1d49
Gus7avoZ <est.gustavo.z@adpp04934.DEFENSORIAPR>
2024-05-21 13:02:43 -0300

    testando git denovo

---
+++ android/app/src/main/kotlin/com/example/testezz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.testezz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bla.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+// import 'package:testezz/main.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+// class Kleber extends StatelessWidget {
+//   const Kleber({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
